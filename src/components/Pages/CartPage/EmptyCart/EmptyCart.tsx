56cc24ddd364d7b2d10bbab8eccc8c6bc2bfceb3
skate090961 <m4xwork@yandex.ru>
2023-10-12 12:34:23 +0400

    add not found page

--- src/components/Pages/CartPage/EmptyCart/EmptyCart.tsx
+++ src/components/Pages/CartPage/EmptyCart/EmptyCart.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import emptyCart from '../../../../assets/img/empty-cart.png';
 import {Link} from "react-router-dom";
+import sadSmile from '../../../../assets/img/sad-smile.png'
 
 export const EmptyCart = () => {
     return (
         <div className="cart cart--empty">
-            <h2>Корзина пустая</h2>
+            <h2>Корзина пустая<img src={sadSmile} alt="sad-smile"/></h2>
             <p>
                 Вероятней всего, вы не заказывали ещё пиццу.<br/>
                 Для того, чтобы заказать пиццу, перейди на главную страницу.
--- src/components/Pages/NotFoundPage/NotFoundPage.tsx
+++ src/components/Pages/NotFoundPage/NotFoundPage.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import s from './NotFoundPage.module.scss'
+import sadSmile from '../../../assets/img/sad-smile.png'
+import {Link} from "react-router-dom";
 
 export const NotFoundPage = () => {
+    console.log(s)
     return (
-        <div>
-            <b>Страница не найдена</b>
+        <div className={s.notFound}>
+            <img src={sadSmile} alt="sad-smile"/>
+            <h1>Страница не найдена</h1>
+            <p>
+                К сожалению, данная страница отсутствует в нашем интернет-магазине.
+            </p>
+            <Link to="/">Вернуться на главную</Link>
         </div>
     );
 };
